2dc4ac923042d5f215ef98cf7254472003965fef
Gomdue <hdh026163@gmail.com>
2025-11-20 23:08:55 +0900

    [황동혁] Implement jenkins deploy API

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from api.routes import temp
+from api.routes import deploy
 
 api_router = APIRouter()
 
 api_router.include_router(temp.router, prefix="/temp", tags=["temp"])
+api_router.include_router(deploy.router, prefix="/deploy", tags=["deploy"])
