b509f49620eb07d6c0269b2727b342dd4ab2309d
hanseul37 <hanseul37@daum.net>
2025-11-23 12:51:58 +0900

    [FIX] error logging

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,8 @@ import uvicorn
 from fastapi import FastAPI
 from api import main
 from starlette.middleware.cors import CORSMiddleware
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 
 app = FastAPI(root_path="/api")
 
@@ -20,3 +22,10 @@ app.include_router(main.api_router)
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+    return JSONResponse(
+        status_code=422,
+        content={"detail": exc.errors(), "body": exc.body}
+    )
