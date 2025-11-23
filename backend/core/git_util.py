f0fbf0f968bd368c23a73c0329794c29f38340cb
hanseul37 <hanseul37@daum.net>
2025-11-23 13:07:23 +0900

    [FIX] private git util

--- backend/core/git_util.py
+++ backend/core/git_util.py
@@ -1,4 +1,5 @@
 import requests
+from typing import Optional
 
 def normalize_repo_url(repo_url: str) -> str:
     repo = repo_url.rstrip("/")
@@ -10,11 +11,15 @@ def normalize_repo_url(repo_url: str) -> str:
     name = parts[1]
     return f"{owner}/{name}"
 
-def get_latest_commit(git_repo: str, branch: str):
+def get_latest_commit(git_repo: str, branch: str, git_pat: Optional[str] = None):
     repo = normalize_repo_url(git_repo)
     url = f"https://api.github.com/repos/{repo}/commits?sha={branch}&per_page=1"
     
-    response = requests.get(url)  # Public repo 기준, 토큰 없이도 가능
+    headers = {}
+    if git_pat:
+        headers["Authorization"] = f"token {git_pat}"
+
+    response = requests.get(url, headers=headers)
     response.raise_for_status()
     
     latest_commit = response.json()[0]
--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,6 @@ import uvicorn
 from fastapi import FastAPI
 from api import main
 from starlette.middleware.cors import CORSMiddleware
-from fastapi.exceptions import RequestValidationError
-from fastapi.responses import JSONResponse
 
 app = FastAPI(root_path="/api")
 
@@ -22,10 +20,3 @@ app.include_router(main.api_router)
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-@app.exception_handler(RequestValidationError)
-async def validation_exception_handler(request, exc):
-    return JSONResponse(
-        status_code=422,
-        content={"detail": exc.errors(), "body": exc.body}
-    )
